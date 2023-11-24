4f010f2dc5f05c4e16e5cec3381bf23d2055e8bf
utsho34 <butsho503@gmail.com>
2023-11-24 23:54:22 +0600

    [ADD] js practice file added

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+// document.write("Hello Utsho");
+console.log("It's done!!!!!!Congrats.....");
+// document.write("JS file is connected.");Utsho Biswas
+console.log("Js connected");
+var x;
+x = prompt("Hi, Enter your name--");
+document.write(x)
+console.log(x)
+const pi = 3.1416
+console.log(pi)
+var a;
+a = 20;
+var b = a + 10;
+console.log(b)
