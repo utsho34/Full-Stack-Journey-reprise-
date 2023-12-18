f8dd7744ec8dfe0bf3408d055ac484f963e107ac
utsho34 <butsho503@gmail.com>
2023-12-18 22:29:04 +0600

    [ADD]practice

---
+++ javascript/hello.js
@@ -0,0 +1,37 @@
+var a = 5;
+var b = 6;
+var c = 3 ;
+
+if(a>b){
+  console.log("A is getter than B");
+}else{
+  console.log("B is getter than A");
+}
+
+if(a%2==0){
+  console.log("A is even number.");
+}
+else{
+  console.log("A is a odd number");
+}
+var st = 'utsho';
+var sp = 'biswas';
+var co = st +" "+ sp;
+console.log(co)
+st_len = st.length
+for(var i=0;i<st_len;i++){
+  console.log(st[i]);
+}
+ar = [1,2,3,4,5,6]
+ar = ar.reverse()
+console.log(ar)
+ar.push(7)
+console.log(ar)
+ar.sort()
+console.log(ar)
+for(var i=8;i<=20;i++){
+  ar.push(i)
+}
+console.log(ar)
+console.log(ar)
+arr=[]
